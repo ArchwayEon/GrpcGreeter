9c0b41cac70b65a60ffd2d0b956deea401bbcff6
Dembe Stephanos <STEPHANOS@ETSU.EDU>
2020-09-23 18:38:38 -0400

    Set up the CustomMathService

---
+++ GrpcGreeter/Services/CustomMathService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcGreeter
+{
+    public class CustomMathService : CustomMath.CustomMathBase
+    {
+        private readonly ILogger<CustomMathService> _logger;
+        public CustomMathService(ILogger<CustomMathService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AddReply> Add(AddRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AddReply
+            {
+                Result = request.Num1 + request.Num2
+            }); ; ;
+        }
+
+        public override Task<SineReply> Sine(SineRequest request, ServerCallContext context)
+        {
+
+            return Task.FromResult(new SineReply
+            {
+                Result = Math.Sin(request.Num1)
+            }); ; ;
+        }
+    }
+}
--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -11,7 +11,8 @@ namespace GrpcGreeterClient
       {
          using var channel = GrpcChannel.ForAddress("https://localhost:5001");
          var client = new Greeter.GreeterClient(channel);
-         var reply = await client.SayHelloAsync(
+
+                        var reply = await client.SayHelloAsync(
             new HelloRequest { Name = "GreeterClient" });
          Console.WriteLine($"Greeting: {reply.Message}");
 
@@ -26,6 +27,16 @@ namespace GrpcGreeterClient
             var greetReply3 = await client.GetGreetingAsync(
         new GreetRequest { Type = 3, Name = "Emma" });
             Console.WriteLine($"Greeting: {greetReply3.Message}");
+
+            var client2 = new CustomMath.CustomMathClient(channel);
+
+            var addReply = await client2.AddAsync(
+        new AddRequest { Num1 = 7F, Num2 = 2F });
+            Console.WriteLine($"Sum: {addReply.Result}");
+
+            var sineReply = await client2.SineAsync(
+        new SineRequest { Num1 = 23.5 });
+            Console.WriteLine($"Sine: {sineReply.Result}");
 
 
             Console.WriteLine("Press any key to exit...");
